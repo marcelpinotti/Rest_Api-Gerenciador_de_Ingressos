f1fc2a1a3ca113501a8f3a01ce13789c81c164f1
marcelpinotti <marcel.pinotti@gmail.com>
2022-06-29 00:02:17 -0300

    Inicializando projeto

---
+++ src/main/java/br/com/marcelpinotti/gerenciadordeingressos/GerenciadorDeIngressosApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcelpinotti.gerenciadordeingressos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeIngressosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeIngressosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcelpinotti/gerenciadordeingressos/GerenciadorDeIngressosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcelpinotti.gerenciadordeingressos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeIngressosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
